ec16247bdf93b82e205c158bc7bd27480b9de02b
jay947462 <jakasofu@bingamton.edu>
2025-09-17 20:44:16 -0400

    added methods for left and right switch states

--- src/states/states.cpp
+++ src/states/states.cpp
@@ -4,7 +4,7 @@ states::states(int RSwitchR, int RSwitchL, int LSwitchR, int LSwitchL, int pot, 
     RSwitchR(RSwitchR), RSwitchL(LSwitchL), LSwitchR(LSwitchR), LSwitchL(LSwitchL), pot(pot), butt(butt)
     {}
 
-states::setup()
+void states::setup()
 {
 
     pinMode(RSwitchR, INPUT);
@@ -17,3 +17,40 @@ states::setup()
 
 }
 
+int states::RSwitchState()
+{
+
+    if (( RSwitchR == HIGH ) && ( RSwitchL == LOW ))
+    {
+        return 2;
+    }
+
+    else if (( RSwitchR == LOW ) && ( RSwitchL == HIGH ))
+    {
+        return 1;
+    }
+
+    else if (( RSwitchR == LOW ) && ( RSwitchL == HIGH ))
+    {
+        return 0;
+    }
+}
+
+int states::LSwitchState()
+{
+
+    if (( LSwitchR == HIGH ) && ( LSwitchL == LOW ))
+    {
+        return 2;
+    }
+
+    else if (( LSwitchR == LOW ) && ( LSwitchL == HIGH ))
+    {
+        return 1;
+    }
+
+    else if (( LSwitchR == LOW ) && ( LSwitchL == HIGH ))
+    {
+        return 0;
+    }
+}
